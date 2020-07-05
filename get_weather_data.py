951ba25f89198e9ea2b69a89cb132e188bc894fb
Aghiya <ghiya.akshat44@gmail.com>
2020-07-05 14:53:40 -0600

    Added frame for current date's data
    New frame added for current date data

--- get_weather_data.py
+++ get_weather_data.py
@@ -16,19 +16,26 @@ import requests
 # Values for getting data.
 API_KEY = api_key
 CITY_ID = 5417598
-URL = 'https://api.openweathermap.org/data/2.5/forecast?id={}&appid={}'.format(CITY_ID, API_KEY)
+CURRENT_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?id={city_id}&appid={api_key}'.format(city_id = CITY_ID,api_key = API_KEY)
+FORECAST_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/forecast?id={city_id}&appid={api_key}'.format(city_id = CITY_ID,api_key =  API_KEY)
 
-def get_weather_forecast():
-    """Get weather data from openweathermap.org, returns in JSON format."""
-    response = requests.get(URL)
+def get_weather_data(url):
+    response = requests.get(url)
     return response.json()
-def get_dates():
-    """Get the next 5 days in the openweathermap format."""
+
+def get_current_date():
+    """Returns the current date in the YYYY-MM-DD format."""
+    curr_date = date.today()
+    return curr_date.strftime('%Y-%m-%d')
+
+def get_forecast_dates():
+    """Returns a list of the next 5 days in the YYYY-MM-DD format."""
     curr_date = date.today()
     dates = []
     for i in range(1, 6):
         dates.append((curr_date + timedelta(days=i)).strftime('%Y-%m-%d'))
     return dates 
+
 def get_forecast_max_and_min_temps(weather_json):
     """Returns specified temperatures in list by three hour blocks for the specified date.
     
@@ -44,10 +51,10 @@ def get_forecast_max_and_min_temps(weather_json):
 
 def get_temp_plot(weather_data_df, forecast_date):
     """
-    Generates a plot of min and max temperature for the given date.
+    Generates a plot of minimum and maximum temperatures at various times for the given date.
     
-    weather_data_df: pandas dataframe of temperatures with the following columns:
-        date, hour, temp_min_F, temp_max_F.
+    weather_data_df: pandas dataframe of temperatures with the following columns: date, hour, temp_min_F, temp_max_F.
+    
     forecast_date: date string formatted to YYYY-MM-DD
     """
     fig = plt.figure(figsize=(5,5), dpi=100)
@@ -60,23 +67,46 @@ def get_temp_plot(weather_data_df, forecast_date):
     plt.tight_layout()
     return fig
 
-def main():
-    weather_json = get_weather_forecast()
-    forecast_dates = get_dates()
+def get_forecast_dates_data():
+    weather_json = get_weather_data(FORECAST_WEATHER_URL)
+    forecast_dates = get_forecast_dates()
     temps_per_date = get_forecast_max_and_min_temps(weather_json)
     weather_data_columns = ['date', 'hour', 'temp_min_K', 'temp_max_K']
     weather_data_df = pd.DataFrame(data=temps_per_date, 
-                                   columns = weather_data_columns)
+                                    columns = weather_data_columns)
     weather_data_df['temp_min_F'] = (weather_data_df['temp_min_K'] - 273.15) * 1.8 + 32
     weather_data_df['temp_max_F'] = (weather_data_df['temp_max_K'] - 273.15) * 1.8 + 32
     temp_plots = []
     for forecast_date in forecast_dates:
         temp_plots.append(get_temp_plot(weather_data_df, forecast_date))
-    
     return temp_plots
     
-
+def get_current_date_data():
+    """Returns the following in a dictionary for the current date from openweathermap: 
+        weather type, weather description, temperature, felt temperature, 
+        humidity, wide speed, and wind direction"""
+        
+    curr_weather_json = get_weather_data(CURRENT_WEATHER_URL)
+    weather = curr_weather_json['weather'][0]['main']
+    weather_desc = curr_weather_json['weather'][0]['description']
+    temp = curr_weather_json['main']['temp']
+    temp_feeling = curr_weather_json['main']['feels_like']
+    humidity = curr_weather_json['main']['humidity']
+    wind_speed = curr_weather_json['wind']['speed']
+    wind_direct = curr_weather_json['wind']['deg']
     
+    curr_weather_data = {'weather' : weather, 
+                         'weather_desc' : weather_desc, 
+                         'temp' : temp, 
+                         'temp_feeling' : temp_feeling, 
+                         'humidity' : humidity, 
+                         'wind_speed' : wind_speed, 
+                         'wind_direct' : wind_direct}
+    return curr_weather_data
+
+def main():
+    return get_current_date_data(), get_forecast_dates_data()
+
 if __name__ == '__main__':
     main()
 
--- weather_gui.py
+++ weather_gui.py
@@ -15,9 +15,10 @@ import tkinter as tk
 import tkinter.font
 
 # Global variables
-initial_city = 'Colorado Springs'
-forecast_dates = get_weather_data.get_dates()
-temperature_plots = get_weather_data.main()
+city = 'Colorado Springs'
+forecast_dates = get_weather_data.get_forecast_dates()
+temperature_plots = get_weather_data.get_forecast_dates_data()
+current_date_data = get_weather_data.get_current_date_data()
 
 # Defines main GUI and every frame inside it.
 class WeatherGui(tk.Tk):
@@ -34,7 +35,7 @@ class WeatherGui(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for F in (Date1, Date2, Date3, Date4, Date5):
+        for F in (CurrDate, Date1, Date2, Date3, Date4, Date5):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
@@ -44,12 +45,53 @@ class WeatherGui(tk.Tk):
             # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame("Date1")
+        self.show_frame("CurrDate")
 
     def show_frame(self, page_name):
         '''Show a frame for the given page name'''
         frame = self.frames[page_name]
         frame.tkraise()
+        
+class CurrDate(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="CurrDate", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        
+        dates_frame = tk.Frame(self)
+        dates_frame.pack()
+        
+        weather_data = tk.Text(self, height = 5, width = 52)
+        weather_data.insert("1.0","The weather is currently " + str(current_date_data['weather_desc']) + ".\n")
+        weather_data.insert("2.0","The temperature is currently " 
+                            + str(current_date_data['temp']) + " K, but it feels like " 
+                            + str(current_date_data['temp_feeling']) + " K.\n")
+        weather_data.insert("3.0", "The humidity is " + str(current_date_data['humidity']) + "%.\n")
+        weather_data.insert("4.0", "The wind is blowing " + str(current_date_data['wind_speed'])
+                            + " at " + str(current_date_data['wind_direct']) + " m/s.\n")
+        
+        button1 = tk.Button(self, text="Current Weather",
+                            command=lambda: controller.show_frame("CurrDate"))
+        button2 = tk.Button(self, text=forecast_dates[0],
+                            command=lambda: controller.show_frame("Date1"))
+        button3 = tk.Button(self, text=forecast_dates[1],
+                            command=lambda: controller.show_frame("Date2"))
+        button4 = tk.Button(self, text=forecast_dates[2],
+                            command=lambda: controller.show_frame("Date3"))
+        button5 = tk.Button(self, text=forecast_dates[3],
+                            command=lambda: controller.show_frame("Date4"))
+        button6 = tk.Button(self, text=forecast_dates[4],
+                            command=lambda: controller.show_frame("Date5"))
+        
+        button1.pack(in_=dates_frame, side='left')
+        button2.pack(in_=dates_frame, side='left')
+        button3.pack(in_=dates_frame, side='left')
+        button4.pack(in_=dates_frame, side='left')
+        button5.pack(in_=dates_frame, side='left')
+        button6.pack(in_=dates_frame, side='left')
+        weather_data.pack()
 
 # Frame 1.
 class Date1(tk.Frame):
@@ -65,15 +107,17 @@ class Date1(tk.Frame):
         
         plot = FigureCanvasTkAgg(temperature_plots[0], self)
 
-        button1 = tk.Button(self, text=forecast_dates[0],
+        button1 = tk.Button(self, text="Current Weather",
+                            command=lambda: controller.show_frame("CurrDate"))
+        button2 = tk.Button(self, text=forecast_dates[0],
                             command=lambda: controller.show_frame("Date1"))
-        button2 = tk.Button(self, text=forecast_dates[1],
+        button3 = tk.Button(self, text=forecast_dates[1],
                             command=lambda: controller.show_frame("Date2"))
-        button3 = tk.Button(self, text=forecast_dates[2],
+        button4 = tk.Button(self, text=forecast_dates[2],
                             command=lambda: controller.show_frame("Date3"))
-        button4 = tk.Button(self, text=forecast_dates[3],
+        button5 = tk.Button(self, text=forecast_dates[3],
                             command=lambda: controller.show_frame("Date4"))
-        button5 = tk.Button(self, text=forecast_dates[4],
+        button6 = tk.Button(self, text=forecast_dates[4],
                             command=lambda: controller.show_frame("Date5"))
         
         button1.pack(in_=dates_frame, side='left')
@@ -81,6 +125,8 @@ class Date1(tk.Frame):
         button3.pack(in_=dates_frame, side='left')
         button4.pack(in_=dates_frame, side='left')
         button5.pack(in_=dates_frame, side='left')
+        button6.pack(in_=dates_frame, side='left')
+        plot.get_tk_widget().pack()
         plot.get_tk_widget().pack()
         
 # Frame 2.
@@ -149,7 +195,6 @@ class Date3(tk.Frame):
         
 # Frame 4.
 class Date4(tk.Frame):
-
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
@@ -177,6 +222,7 @@ class Date4(tk.Frame):
         button3.pack(in_=dates_frame, side='left')
         button4.pack(in_=dates_frame, side='left')
         button5.pack(in_=dates_frame, side='left')
+        plot.get_tk_widget().pack()
         plot.get_tk_widget().pack()
         
 # Frame 5.
